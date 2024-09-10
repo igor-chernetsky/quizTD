23dd86f48055d399c905ac7471875f6ea50e1f12
igor-chernetsky <ihar.charnetski@smartvalor.com>
2024-09-10 16:47:26 +0300

    geo translation is added

--- lib/widget/infoWidgets/buildingInfoWidget.dart
+++ lib/widget/infoWidgets/buildingInfoWidget.dart
@@ -5,11 +5,27 @@ import 'package:quiz_defence/models/building_model.dart';
 import 'package:quiz_defence/utils/colors.dart';
 import 'package:quiz_defence/widget/infoWidgets/closeDialogButton.dart';
 import 'package:quiz_defence/widget/playgroundWidgets/buildingWidget.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class BuildingInfo extends StatelessWidget {
   final BuildingModel building;
   final int epoch;
   const BuildingInfo({super.key, required this.building, required this.epoch});
+
+  getDescription(AppLocalizations locale, BuildingType? t) {
+    if (t != null) {
+      switch (t) {
+        case BuildingType.farm:
+          return locale.descriptionFarm;
+        case BuildingType.main:
+          return locale.descriptionMain;
+        case BuildingType.school:
+          return locale.descriptionSchool;
+        case BuildingType.tower:
+          return locale.descriptionTower;
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,7 +52,9 @@ class BuildingInfo extends StatelessWidget {
               ),
               const SizedBox(width: 20),
               Expanded(
-                child: Text(buldingDescription[building.type]!,
+                child: Text(
+                    getDescription(
+                        AppLocalizations.of(context)!, building.type),
                     style: TextStyle(
                         color: AppColors.textSecondary, fontSize: 16)),
               ),
